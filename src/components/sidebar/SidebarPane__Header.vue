e1033c97a2a1da185a2ce8bf0a25dd793aa59ae1
jeremylim91 <jeremylim_91@live.com>
2021-05-19 15:09:33 +0800

    changed css

--- src/components/sidebar/SidebarPane__Header.vue
+++ src/components/sidebar/SidebarPane__Header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sidebar-header">
-    This is the Header
+    <button type="button"><img src="../../assets/profilePic.png" /></button>
+    <button type="button"><img src="../../assets/addContact.png" /></button>
+    <button type="button"><img src="../../assets/messageAdd.png" /></button>
   </div>
 </template>
 <script>
@@ -10,7 +12,20 @@ export default {};
 <style>
 .sidebar-header {
   border: 1px solid black;
-  height: 3em;
-  margin-bottom: 10px;
+  margin: 0px -10px 10px;
+  overflow: hidden;
+  background-color: lightgoldenrodyellow;
+  padding: 0.2em 0.2em 0.2em;
+  /* display: flex;
+  justify-content: flex-start; */
+}
+img {
+  height: 2em;
+  vertical-align: middle;
+}
+button {
+  background-color: transparent;
+  border: none;
+  vertical-align: middle;
 }
 </style>
