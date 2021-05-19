3bca0a52718e9c8c5cda6334a779b008f7cf2833
jeremylim91 <jeremylim_91@live.com>
2021-05-19 15:07:28 +0800

    changed css

--- src/components/sidebar/SidebarPane__Chats.vue
+++ src/components/sidebar/SidebarPane__Chats.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <div class="chat-summary" v-for="chat in sampleChats" :key="chat.name">
-      <div class="name">{{ chat.name }}</div>
-      <div class="latestmsg">{{ chat.latestMsg }}</div>
-    </div>
+  <div class="container">
+    <button
+      type="button"
+      class="chat-summary"
+      v-for="chat in sampleChats"
+      :key="chat.name"
+    >
+      <div>{{ chat.avatar }}</div>
+      <div>
+        <div class="name">{{ chat.name }}</div>
+        <div class="latestmsg">{{ chat.latestMsg }}</div>
+      </div>
+    </button>
   </div>
 </template>
 <script>
@@ -13,7 +21,6 @@ export default {
       sampleChats: [
         { name: "Joel", avatar: ":)", latestMsg: "sup bro" },
         { name: "Justin", avatar: ":)", latestMsg: "Eh wanna go lunch?" }
-        //
       ]
     };
   }
@@ -21,9 +28,16 @@ export default {
 </script>
 <style scoped>
 .chat-summary {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+  width: 100%;
   margin: 0.5em 0.2em 0.5em;
-  border-bottom: 0.1em solid lightgrey;
   overflow: scroll;
+  height: 3em;
+  /* override original button styles */
+  border: none;
+  border-bottom: 0.2em solid lightgrey;
+  text-align: left;
 }
 .chat-summary .name {
   font-weight: 600;
