b935afe19436207b68bb722dc3bcae46d466d02b
jeremylim91 <jeremylim_91@live.com>
2021-05-19 15:06:58 +0800

    changed css-- padding for sidebar-pane

--- src/components/sidebar/SidebarPane.vue
+++ src/components/sidebar/SidebarPane.vue
@@ -19,7 +19,7 @@ export default {
 </script>
 <style scoped>
 .sidebar-pane {
-  padding: 20px 10px 20px;
+  padding: 0px 10px 20px;
   width: 10em;
   background-color: lightblue;
   height: 100vh;
