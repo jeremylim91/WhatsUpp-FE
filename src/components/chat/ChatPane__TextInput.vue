cf62ee863de033cc1314482efe558a25be9ca684
jeremylim91 <jeremylim_91@live.com>
2021-05-20 22:48:27 +0800

    added the text input box and styled it

--- src/components/chat/ChatPane__TextInput.vue
+++ src/components/chat/ChatPane__TextInput.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>
-    This is TextInput
+  <div class="text-input-container">
+    <input type="text" class="input-box" v-model="textInput" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      textInput: ""
+    };
+  }
+};
 </script>
+<style scoped>
+.text-input-container {
+  position: absolute;
+  bottom: 0px;
+  height: 3em;
+  background-color: lightsalmon;
+  width: 100%;
+  margin: 0px -10px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.input-box {
+  font-size: 1em;
+  height: 70%;
+  width: 90%;
+  border-radius: 50px;
+  padding: 0px 0.5em 0px;
+}
+</style>
