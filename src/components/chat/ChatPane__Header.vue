0997d2d7b2d1ff751e8088cae586e96fb3d4e5ab
jeremylim91 <jeremylim_91@live.com>
2021-05-20 22:47:57 +0800

    added css

--- src/components/chat/ChatPane__Header.vue
+++ src/components/chat/ChatPane__Header.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    This is Header
+  <div class="header">
+    <div>{{ name }}</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "chat name"
+    };
+  }
+};
 </script>
+<style scoped>
+.header {
+  border: 1px solid black;
+  margin: 0px -10px 10px;
+  overflow: hidden;
+  background-color: lightgoldenrodyellow;
+  padding: 0.2em 1em 0.2em;
+  height: 2.5em;
+  display: flex;
+  align-items: center;
+}
+</style>
