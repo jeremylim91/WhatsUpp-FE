2f56c6a817caea4d8465faf29e9c35516683e62c
jeremylim91 <jeremylim_91@live.com>
2021-06-02 08:01:10 +0800

    added utils to hold helper fns

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
-import User from './components/User.vue'
-import VueRouter from 'vue-router'
-import Homepage from './components/Homepage.vue'
-import Login from './components/Login.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import User from "./components/User.vue";
+import VueRouter from "vue-router";
+import Homepage from "./components/Homepage.vue";
+import Login from "./components/Login.vue";
 
 // Tell app to use Vue router for this single-page application
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // specify routes and associated UI component
-const routes=[
-  {path:'/login', component:Login},
-  {path:'/', component: Homepage}
-]
+const routes = [
+  { path: "/login", component: Login },
+  { path: "/", component: Homepage }
+];
 // create a VueRouter Instance and use the above info to tell app to route the accordingly
-const router= new VueRouter({
+const router = new VueRouter({
   // the value (i.e. 'routes) referse to the routes constant defined above
   routes: routes
-})
+});
 
-Vue.component('app-user', User)
+Vue.component("app-user", User);
 new Vue({
-  el: '#app',
+  el: "#app",
   router: router,
   render: h => h(App)
-})
+});
---
+++ src/utils.mjs
@@ -0,0 +1,8 @@
+// filterChatsByName = (query, chats)=>(
+// chats.filter((chat)=>{
+// if (chat.name.search(query)=== -1){
+//   return false
+// }
+// return true
+// })
+// )
