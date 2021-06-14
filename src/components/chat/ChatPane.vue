3a57e04c0698f5fb92d3a199ea9ab89adc13a906
jeremylim91 <jeremylim_91@live.com>
2021-06-14 12:07:08 +0800

    integrated vuex store

--- src/components/chat/ChatPane.vue
+++ src/components/chat/ChatPane.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="chat-pane">
-    <chat-pane__header />
-    <chat-pane__chat />
-    <chat-pane__text-input />
+    <div v-if="selectedRoom">
+      <chat-pane__header />
+      <chat-pane__chat />
+      <chat-pane__text-input />
+    </div>
+    <div v-else>Select on a chat room to begin</div>
   </div>
 </template>
 
@@ -10,8 +13,14 @@
 import ChatPane__Header from "./ChatPane__Header";
 import ChatPane__Chat from "./ChatPane__Chat";
 import ChatPane__TextInput from "./ChatPane__TextInput";
+import { mapGetters } from "vuex";
 
 export default {
+  computed: {
+    ...mapGetters({
+      selectedRoom: "getSelectedRoom"
+    })
+  },
   components: {
     "chat-pane__header": ChatPane__Header,
     "chat-pane__chat": ChatPane__Chat,
--- src/components/chat/ChatPane__Chat.vue
+++ src/components/chat/ChatPane__Chat.vue
@@ -1,9 +1,98 @@
 <template>
-  <div>
-    This is Chat
+  <div class="container">
+    <div
+      v-for="message in chatContents"
+      class="message-field"
+      :class="alignMsgs(message)"
+      :key="message.id"
+    >
+      <!-- :class="{ myMsg: message.username === sessionDetails.username }" -->
+      <!-- <div class="test1"> -->
+      <span class="username-field">{{ message.username }}</span>
+      <span class="message-box">
+        <div>{{ message.message }}</div>
+        <div class="timestamp">{{ getTime(message.timestamp) }}</div>
+      </span>
+    </div>
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
-export default {};
+import moment from "moment";
+import { mapGetters } from "vuex";
+import { io } from "socket.io-client";
+
+export default {
+  data() {
+    return {
+      isMyMsg: false
+    };
+  },
+  computed: {
+    ...mapGetters({
+      chatContents: "getChatContents",
+      selectedRoom: "getSelectedRoom",
+      sessionDetails: "getSessionDetails"
+    })
+  },
+  watch: {
+    // both args below are provided automatically by vue
+    // new value is the latest value of the state
+    selectedRoom(newValue, oldValue) {
+      this.$store.dispatch("fetchChatContents");
+    }
+  },
+  mounted() {
+    // make an axios request to get all the chats and update the state on which chat is open
+    this.$store.dispatch("fetchChatContents");
+    //
+  },
+  methods: {
+    // if the user created the message, align it to the right of the pane; else align left
+    alignMsgs(message) {
+      if (message.username === this.sessionDetails.username) {
+        return "myMsg";
+      }
+      return;
+    },
+    getTime(timestamp) {
+      return moment(timestamp).format("h:mma");
+    }
+  }
+};
 </script>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.message-field {
+  display: flex;
+  flex-direction: column;
+  margin: 0.5em 0em 0.5em;
+  font-weight: 600;
+  font-size: 0.6rem;
+}
+
+.test1 {
+  display: inline-flex;
+  flex-direction: column;
+}
+.message-box {
+  display: inline-flex;
+  font-weight: 500;
+  font-size: 1rem;
+  flex-direction: column;
+  background-color: lightgrey;
+  border-radius: 15px;
+  padding: 0.2em 0.5em 0.2em;
+  max-width: 15em;
+}
+.myMsg {
+  align-self: flex-end;
+}
+.timestamp {
+  font-size: 0.6rem;
+}
+</style>
