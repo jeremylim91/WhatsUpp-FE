9ce239b9565960483e7839c95d68c5cc316e043a
jeremylim91 <jeremylim_91@live.com>
2021-06-14 12:03:21 +0800

    commented out unnec code

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: "app",
-  // "the value stored in the data property is a function"
-  data() {
-    return {
-      msg: "This is 'msg' in the data obj"
-    };
-  }
+  // name: "app",
+  //   // "the value stored in the data property is a function"
+  //   data() {
+  //     return {
+  //       msg: "This is 'msg' in the data obj"
+  //     };
+  //   }
 };
 </script>
 <style>
