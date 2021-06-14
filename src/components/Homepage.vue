24cbcefa5bffaf66096b3f770fa9f7d5f18adc76
jeremylim91 <jeremylim_91@live.com>
2021-06-14 12:05:27 +0800

    integrated vuex store

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -8,8 +8,18 @@
 <script>
 import SidebarPane from "./sidebar/SidebarPane";
 import ChatPane from "./chat/ChatPane";
+import { mapActions } from "vuex";
 
 export default {
+  // methods: {
+  //   ...mapActions(["fetchUserChatHistory"])
+  // },
+  // created() {
+  //   this.fetchUserChatHistory;
+  // },
+  mounted() {
+    this.$store.dispatch("fetchUserChatHistory");
+  },
   components: {
     "sidebar-pane": SidebarPane,
     "chat-pane": ChatPane
