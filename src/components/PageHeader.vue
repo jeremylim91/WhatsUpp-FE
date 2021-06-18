3fec7ce2c04893ed103d6610a62781a56d4309ba
jeremylim91 <jeremylim_91@live.com>
2021-06-18 14:35:07 +0800

    added a new UI component and moved the respective headers into this larger header component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page-header">
+    <div>This is header content</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.page-header {
+  grid-area: header;
+}
+</style>
