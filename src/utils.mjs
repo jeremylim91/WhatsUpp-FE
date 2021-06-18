2c2e06b7a625dee78ce3543061e7920429d3dfff
jeremylim91 <jeremylim_91@live.com>
2021-06-18 14:32:24 +0800

    removed unnecssary fns

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,8 +1,28 @@
-// filterChatsByName = (query, chats)=>(
-// chats.filter((chat)=>{
-// if (chat.name.search(query)=== -1){
-//   return false
-// }
-// return true
-// })
-// )
+export const getUsernameFromCookie = () => {
+  if (document.cookie) {
+    const splitCookieVal = document.cookie.split(" ");
+    const usernameStartPos = splitCookieVal[0].indexOf("=") + 1;
+    const usernameEndPos = splitCookieVal[0].indexOf(";");
+    const loggedInUsername = splitCookieVal[0].substring(
+      usernameStartPos,
+      usernameEndPos
+    );
+    return loggedInUsername;
+  }
+  return null;
+};
+
+// function to get userid; assumes that userId was the second cookie that was set when user logged in
+export const getUserIdFromCookie = () => {
+  if (document.cookie) {
+    const splitCookieVal = document.cookie.split(" ");
+    const userIdStartPos = splitCookieVal[1].indexOf("=") + 1;
+    const userIdEndPos = splitCookieVal[1].indexOf(";");
+    const loggedInUserId = splitCookieVal[1].substring(
+      userIdStartPos,
+      userIdEndPos
+    );
+    return Number(loggedInUserId);
+  }
+  return null;
+};
