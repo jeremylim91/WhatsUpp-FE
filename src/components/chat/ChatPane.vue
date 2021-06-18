41bbcc65b37e4421a1c2c76f8a153a487207f2ad
jeremylim91 <jeremylim_91@live.com>
2021-06-18 14:40:05 +0800

    changed code to control conditional rendering

--- src/components/chat/ChatPane.vue
+++ src/components/chat/ChatPane.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="chat-pane">
-    <div v-if="selectedRoom">
-      <chat-pane__header />
-      <chat-pane__chat />
-      <chat-pane__text-input />
-    </div>
+    <!-- <chat-pane__chat /> -->
+    <chat-pane-chat v-if="selectedRoom"></chat-pane-chat>
+    <chat-pane-text-input v-if="selectedRoom"></chat-pane-text-input>
     <div v-else>Select on a chat room to begin</div>
   </div>
 </template>
 
 <script>
-import ChatPane__Header from "./ChatPane__Header";
-import ChatPane__Chat from "./ChatPane__Chat";
-import ChatPane__TextInput from "./ChatPane__TextInput";
+import ChatPaneChat from "./ChatPane__Chat";
+import ChatPaneTextInput from "./ChatPane__TextInput";
 import { mapGetters } from "vuex";
 
 export default {
@@ -22,19 +19,20 @@ export default {
     })
   },
   components: {
-    "chat-pane__header": ChatPane__Header,
-    "chat-pane__chat": ChatPane__Chat,
-    "chat-pane__text-input": ChatPane__TextInput
+    ChatPaneChat,
+    ChatPaneTextInput
   }
 };
 </script>
 
 <style scoped>
-.chat-pane {
+/* .chat-pane {
   padding: 0px 10px 20px;
   width: 100vw !important;
   background-color: lightpink;
-  height: 100vh;
-  position: relative;
-}
+  height: 100vh; */
+/* position: relative; */
+/* display: flex;
+  flex: 1;
+} */
 </style>
